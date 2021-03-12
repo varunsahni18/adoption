0bb7409215b5b95aa95fd8990a56811a0ba2e4d0
Varun Sahni <74827870+varunsahni18@users.noreply.github.com>
2021-03-13 01:30:50 +0530

    Create image.dart

---
+++ image.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Box extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Card(
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onTap: () {
+            print('Card tapped.');
+          },
+          child: Column(
+            children: [
+              Image.network(
+                  'https://www.cdc.gov/healthypets/images/pets/cute-dog-headshot.jpg',
+                  // width: 300,
+                  height: 150,
+                  fit: BoxFit.fill),
+              Container(
+                width: 200,
+                child: ListTile(
+                  title: Text(
+                    'Poodle',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontFamily: 'Futura',
+                      color: Colors.blue,
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.verified_outlined,
+                    color: Colors.green,
+                    size: 30,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
